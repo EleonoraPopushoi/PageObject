28b6b6a9f344c187bd478d2d69b89f181f05ba61
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-11-14 18:50:48 +0200

    tests were added

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,18 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement verificationField = $("input[name=\"code\"]");
+    private SelenideElement verificationButton = $("[data-test-id=action-verify]");
+
+    public DashboardPage verify(DataHelper.VerificationCode code) {
+        verificationField.setValue(code.getCode());
+        verificationButton.click();
+        return new DashboardPage();
+    }
+}
