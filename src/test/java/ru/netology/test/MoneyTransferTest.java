722d5d7b7cba942f83deff66b95a2f3eef7a1e11
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-11-18 20:06:34 +0200

    test was modefited

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,14 +1,12 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
+
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,7 +14,7 @@ public class MoneyTransferTest {
 
     @Test
     void shouldTransferFromFirstToSecond() {
-        int amount = 300;
+        int amount = 400;
         val loginPage = open("http://localhost:9999", LoginPage.class);
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
@@ -67,7 +65,7 @@ public class MoneyTransferTest {
         val transferPage = dashboardPage.firstCard();
         val cardInfo = DataHelper.getSecondCardInfo();
         transferPage.transferCard(cardInfo, amount);
-        $(withText("На балансе недостаточно средств")).shouldBe(Condition.visible);
+        transferPage.checkNotification();
     }
 }
 
