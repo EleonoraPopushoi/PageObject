71f54f8bcff5c2b9710f48b19f8b85f078ed8c50
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-11-18 20:08:09 +0200

    cssSelectors were renewed

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -2,39 +2,31 @@ package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.conditions.Text;
 import ru.netology.data.DataHelper;
 
-import javax.annotation.CheckReturnValue;
-import javax.annotation.Nonnull;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
-    private SelenideElement amountField = $("[data-test-id=\"amount\"] input");
-    private SelenideElement fromField = $("[data-test-id=from] input");
-    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement amountField = $("[data-test-id='amount'] input");
+    private SelenideElement fromField = $("[data-test-id='from'] input");
+    private SelenideElement transferButton = $("[data-test-id='action-transfer']");
 
     public void transferCard(DataHelper.CardInfo CardInfo, int amountToTransfer) {
         amountField.setValue(String.valueOf(amountToTransfer));
         fromField.setValue(CardInfo.getCardNumber());
         transferButton.click();
     }
-
-    @CheckReturnValue
-    @Nonnull
-    public static Condition withText(String text) {
-        return new Text("На балансе недостаточно средств");
+    public void checkNotification() {
+        $(withText("На балансе недостаточно средств")).shouldBe(Condition.visible);
     }
+
 }
 
+//@CheckReturnValue
+//    @Nonnull
+//    public static Condition withText(String text) {
+//        return new Text("На балансе недостаточно средств");
+//    }
 
-// public void overTransferThanCurrentBalance(DataHelper.CardInfo CardInfo, int amountToTransfer) {
-//            amountField.setValue(String.valueOf(amountToTransfer));
-//        if () {
-//            fromField.setValue(CardInfo.getCardNumber());
-//            transferButton.click();
-//        else() {
-//                System.out.println("На балансе недостаточно средств");
-//                return (transferCard);
-//            }
--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -7,8 +7,8 @@ import ru.netology.page.DashboardPage;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private SelenideElement verificationField = $("[data-test-id=\"code\"]input");
-    private SelenideElement verificationButton = $("[data-test-id=action-verify]");
+    private SelenideElement verificationField = $("[data-test-id='code'] input");
+    private SelenideElement verificationButton = $("[data-test-id='action-verify']");
 
     public DashboardPage verify(DataHelper.VerificationCode code) {
         verificationField.setValue(code.getCode());
